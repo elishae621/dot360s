95d917e333b661a90b9571ed0d6101c743774a1b
Emmanuel Elisha <elishae621@gmail.com>
2020-10-15 22:31:39 +0100

    update

--- user/models.py
+++ user/models.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.shortcuts import reverse
+from phonenumber_field.modelfields import PhoneNumberField
+from django.utils import timezone
 
 
 class CustomAccountManager(BaseUserManager):
@@ -24,6 +26,7 @@ class CustomAccountManager(BaseUserManager):
         other_fields.setdefault('is_superuser', False)
         other_fields.setdefault('is_staff', False)
         other_fields.setdefault('is_active', True)
+        other_fields.setdefault('is_driver', False)
 
         return self._create_user(email, firstname, lastname, password, **other_fields)
 
@@ -32,6 +35,7 @@ class CustomAccountManager(BaseUserManager):
         other_fields.setdefault('is_staff', True)
         other_fields.setdefault('is_superuser', True)
         other_fields.setdefault('is_active', True)
+        other_fields.setdefault('is_driver', False)
 
         if other_fields.get('is_staff') is not True:
             raise ValueError('Superuser must be assigned to is_staff=True.')
@@ -49,6 +53,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     lastname = models.CharField(max_length=150, blank=True, null=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_driver = models.BooleanField(default=False)
+    phone = PhoneNumberField(null=True, blank=False, unique=True)
 
     objects = CustomAccountManager()
 
@@ -72,3 +78,88 @@ class Profile(models.Model):
 
     def get_absolute_url(self):
         return f'/user/{self.pk}/'
+
+
+location = [
+    ('UMUAHIA', 'Umuahia'),
+    ('ABA', 'Aba')
+]
+
+journey_choice = [
+    ('IN', 'Within the city'),
+    ('OUT', 'InterCity travel')
+]
+
+vehicle_choice = [
+    ('T', 'Tricycle'),
+    ('B', 'Bus'), 
+    ('C', 'Car'),
+]
+
+
+class Driver(User):
+    polar_choices = [
+        ('not_avaliable', 'Not Avaliable'),
+        ('busy', 'Busy'),
+        ('avaliable', 'Avaliable'),
+    ]
+    username = models.CharField(max_length=10)
+    location = models.CharField(max_length=15, choices=location,
+        null=True, blank=True)
+    about = models.TextField(default="No about yet for this driver")
+    status = models.CharField(max_length=13, choices=polar_choices, default='not_avaliable')
+    journey_type = models.CharField(max_length=3, choices=journey_choice,
+        default='IN')
+
+    def __str__(self):
+        return f'Driver: {self.username}'
+
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=20)
+    plate_number = models.CharField(max_length=15)
+    capacity = models.IntegerField()
+    vehicle_type = models.CharField(max_length=1, choices=vehicle_choice, 
+        null=True, blank=True)
+    owner = models.ForeignKey(Driver, on_delete=models.CASCADE)
+
+
+class Passenger(User):
+    """same as the base user for now"""
+
+    def __str__(self):
+        return f'Passenger: {self.email}'
+    
+
+
+class Request(models.Model):
+    driver = models.ForeignKey(Driver, on_delete=models.SET_NULL, null=True)
+    passenger = models.ForeignKey(Passenger, on_delete=models.SET_NULL, null=True)
+    from_address = models.CharField(max_length=70)
+    to_address = models.CharField(max_length=70)
+    city = models.CharField(max_length=15, choices=location,
+        null=True, blank=True)
+    no_of_passengers = models.IntegerField(default=1)
+    load = models.BooleanField(default=False)
+    time = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f'Request: {self.driver}, {self.passenger}'
+
+
+ride_status = [
+    ('WC', 'Waiting for confirmation'),
+    ('WD', 'Waiting for driver'),
+    ('ON', 'Ongoing'),
+    ('CO', 'Completed'),
+]
+
+class Ride(models.Model):
+    request = models.OneToOneField(Request, on_delete=models.CASCADE)
+    status = models.CharField(max_length=2, choices=ride_status, 
+        null=True, blank=True)
+    price = models.FloatField(default=100.00)
+
+    def __str__(self):
+        return f'Ride => {self.request}'
+    
--- user/tests/test_models.py
+++ user/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from user.models import User, Profile
+from user.models import User, Profile, Driver, Vehicle, Passenger, Request, Ride
 from dot360s.settings import INSTALLED_APPS, AUTH_USER_MODEL
 from django.db import transaction
 from django.shortcuts import reverse
@@ -80,3 +80,60 @@ class TestProfileModel(TestCase):
     def test_get_absolute_url(self):
         self.assertEqual(self.user.profile.get_absolute_url(), reverse(
             'profile_detail', kwargs={'pk': self.user.pk}))
+
+
+class TestDriver(TestCase):
+    def setUp(self):
+        self.faker = Faker()
+        self.driver = Driver.objects.create(email=self.faker.email(),
+            firstname=self.faker.first_name(), lastname=self.faker.last_name(),
+            username=self.faker.user_name(), is_driver=True)
+        
+    def test_str_function(self):
+        self.assertEqual(str(self.driver), f"Driver: {self.driver.username}")
+
+
+class TestPassenger(TestCase):
+    def setUp(self):
+        self.faker = Faker()
+        self.passenger = Passenger.objects.create(email=self.faker.email(),
+            firstname=self.faker.first_name(), lastname=self.faker.last_name())
+
+    def test_str_function(self):
+        self.assertEqual(str(self.passenger), f"Passenger: {self.passenger.email}")
+
+
+class TestRequest(TestCase):
+    def setUp(self):
+        self.faker = Faker()
+        self.driver = Driver.objects.create(email=self.faker.email(),
+            firstname=self.faker.first_name(), lastname=self.faker.last_name(),
+            username=self.faker.user_name())
+        self.passenger = Passenger.objects.create(email=self.faker.email(),
+            firstname=self.faker.first_name(), lastname=self.faker.last_name())
+
+        self.request = Request.objects.create(driver=self.driver, 
+            passenger=self.passenger, from_address="address", to_address="address")
+    
+    def test_str_function(self):
+        self.assertEqual(str(self.request), 
+            f"Request: {self.driver}, {self.passenger}")
+
+
+
+class TestRide(TestCase):
+    def setUp(self):
+        self.faker = Faker()
+        self.driver = Driver.objects.create(email=self.faker.email(),
+            firstname=self.faker.first_name(), lastname=self.faker.last_name(),
+            username=self.faker.user_name())
+        self.passenger = Passenger.objects.create(email=self.faker.email(),
+            firstname=self.faker.first_name(), lastname=self.faker.last_name())
+        self.request = Request.objects.create(driver=self.driver, 
+            passenger=self.passenger, from_address="address", to_address="address")
+        self.ride = Ride.objects.create(request=self.request)
+
+    def test_str_function(self):
+        self.assertEqual(str(self.ride), 
+            f"Ride => Request: {self.driver}, {self.passenger}")
+
