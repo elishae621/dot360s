03375a3a8d3fccf30495fd0c15d88e243b33445d
Emmanuel Elisha <elishae621@gmail.com>
2020-11-06 01:11:22 +0100

    update

--- dot360s/settings.py
+++ dot360s/settings.py
@@ -159,6 +159,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = PurePath.joinpath(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = PurePath.joinpath(BASE_DIR, 'media')
